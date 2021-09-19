77ac28df32e4f40ddb547051eafddfae03baeaf2
ahmed khaled <ahmed@mrkhalednasr.com>
2021-09-19 18:28:26 +0200

    adding more products

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,72 +1,141 @@
-  
+
 const products = [
-    {
-      name: "PlayStation 5",
-      imageUrl:       
-       "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
-      description:
-        "PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. Announced in 2019 as the successor to the PlayStation 4, the PS5 was released on November 12, 2020 in Australia, Japan, New Zealand, North America, Singapore, and South Korea, and November 19, 2020 onwards in other major markets except China and India.",
-      price: 499,
-      countInStock: 15,
-      rating:3,
-      numReviews:10,
-    },
-    {
-      name: "Iphone 12",
-      imageUrl:        
+  {
+    name: "PlayStation 5",
+    imageUrl:
+      "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
+    description:
+      "PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. Announced in 2019 as the successor to the PlayStation 4, the PS5 was released on November 12, 2020 in Australia, Japan, New Zealand, North America, Singapore, and South Korea, and November 19, 2020 onwards in other major markets except China and India.",
+    price: 499,
+    countInStock: 15,
+    rating: 3,
+    numReviews: 10,
+  },
+  {
+    name: "Iphone 12",
+    imageUrl:
       "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
-      description:
-        "Welcome to a new era of iPhone. Beautifully bright 6.1-inch Super Retina XDR display.1 Ceramic Shield with 4x better drop performance.2 Incredible low-light photography with Night mode on all cameras. Cinema-grade Dolby Vision video recording, editing, and playback. Powerful A14 Bionic chip. And new MagSafe accessories for easy attach and faster wireless charging.3 Let the fun begin.",
-      price: 1099,
-      countInStock: 10,
-      rating:5,
-      numReviews:7,
-    },
-    {
-      name: "Cannon EOS-1D",
-      imageUrl:        
+    description:
+      "Welcome to a new era of iPhone. Beautifully bright 6.1-inch Super Retina XDR display.1 Ceramic Shield with 4x better drop performance.2 Incredible low-light photography with Night mode on all cameras. Cinema-grade Dolby Vision video recording, editing, and playback. Powerful A14 Bionic chip. And new MagSafe accessories for easy attach and faster wireless charging.3 Let the fun begin.",
+    price: 1099,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 7,
+  },
+  {
+    name: "Cannon EOS-1D",
+    imageUrl:
       "https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-      description:
-        "The EOS-1D X combines speed with image quality, to create the next generation camera for professionals. Full frame 18 megapixel sensor with Dual “DIGIC 5+” processors sets the standard, and up to 12 frames per second shooting takes it beyond.",
-      price: 1300,
-      countInStock: 5,
-      rating:2,
-      numReviews:3,
-    },
-    {
-      name: "Amazon Alexa",
-      imageUrl:        
+    description:
+      "The EOS-1D X combines speed with image quality, to create the next generation camera for professionals. Full frame 18 megapixel sensor with Dual “DIGIC 5+” processors sets the standard, and up to 12 frames per second shooting takes it beyond.",
+    price: 1300,
+    countInStock: 5,
+    rating: 2,
+    numReviews: 3,
+  },
+  {
+    name: "Amazon Alexa",
+    imageUrl:
       "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
-      description:
-        "It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system.",
-      price: 50,
-      countInStock: 25,
-      rating:4,
-      numReviews:15,
-    },
-    {
-      name: "Audio Technica Headphones",
-      imageUrl:        
+    description:
+      "It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system.",
+    price: 50,
+    countInStock: 25,
+    rating: 4,
+    numReviews: 15,
+  },
+  {
+    name: "Audio Technica Headphones",
+    imageUrl:
       "https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-      description:
-        "Outfitted with 45mm large-aperture dynamic drivers and an over-ear, closed-back design, the ATH-M50x headphones deliver clarity, deep bass, and extended bandwidth (15 Hz to 28 kHz) while isolating you from outside sounds.",
-      price: 233,
-      countInStock: 4,
-      rating:1,
-      numReviews:2,
-    },
-    {
-      name: "JBL FLIP 4",
-      imageUrl:        
+    description:
+      "Outfitted with 45mm large-aperture dynamic drivers and an over-ear, closed-back design, the ATH-M50x headphones deliver clarity, deep bass, and extended bandwidth (15 Hz to 28 kHz) while isolating you from outside sounds.",
+    price: 233,
+    countInStock: 4,
+    rating: 1,
+    numReviews: 2,
+  },
+  {
+    name: "JBL FLIP 4",
+    imageUrl:
       "https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
 
-      description:
-        "JBL Flip 4 is the next generation in the award-winning Flip series; it is a portable Bluetooth speaker that delivers surprisingly powerful stereo sound. This compact speaker is powered by a 3000mAh rechargeable Li-ion battery that offers up to 12 hours of continuous, high-quality audio playtime.",
-      price: 140,
-      countInStock: 10,
-      rating:3,
-      numReviews:9,
-    },
-  ];
-  
-  module.exports = products;
+    description:
+      "JBL Flip 4 is the next generation in the award-winning Flip series; it is a portable Bluetooth speaker that delivers surprisingly powerful stereo sound. This compact speaker is powered by a 3000mAh rechargeable Li-ion battery that offers up to 12 hours of continuous, high-quality audio playtime.",
+    price: 140,
+    countInStock: 10,
+    rating: 3,
+    numReviews: 9,
+  },
+  {
+    name: "Apple smart watch ",
+    imageUrl:
+      "https://images.unsplash.com/photo-1533675180235-0ba57d720b51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80",
+
+    description:
+      "it is a smart watch which  has many exciting features as call,music ,clock,health monitoring apps",
+    price: 3440,
+    countInStock: 18,
+    rating: 3,
+    numReviews: 11
+  },
+  {
+    name: "lenovo Thinkpad",
+    imageUrl:
+      "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1400&q=80",
+    description:
+      "Lenovo E14 20RA000KED ThinkPad Laptop, 14 Inch FHD, Intel Core i7-10510U, 1 TB, 8 GB RAM, 2GB AMD Radeon RX 640 Graphics, DOS - Black",
+    price: 17000,
+    countInStock: 7,
+    rating: 5,
+    numReviews: 8
+  },
+
+  {
+    name: "Macbook Pro",
+    imageUrl:
+      "https://images.unsplash.com/5/unsplash-kitsune-3.jpg?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80",
+
+    description: "Apple MacBook Pro is a macOS laptop with a 13.30-inch display that has a resolution of 2560x1600 pixels. It is powered by a Core i5 processor and it comes with 12GB of RAM. The Apple MacBook Pro packs 512GB of SSD storage.",
+    price: 40000,
+    countInStock: 9,
+    rating: 4,
+    numReviews: 5
+
+  },
+
+  {
+    name: "Huawei Band 4",
+    imageUrl:
+      "https://images.unsplash.com/photo-1521445441669-8d7d07586439?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
+    description: "it is a sport's band that measures the health condition while practicing any kind of sports",
+    price: 558,
+    countInStock: 8,
+    rating: 3,
+    numReviews: 16
+
+  },
+  {
+    name: "Apple airpods",
+    imageUrl:
+      "https://images.unsplash.com/photo-1617814175955-24521459cb64?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    description: "Apple airpods, True Wireless Earphones /Intelligent Active Noise Canceling, black",
+    price: 1844,
+    countInStock: 7,
+    rating: 4,
+    numReviews: 9
+  },
+  {
+    name: "Apple wireless mouse",
+    imageUrl:
+      "https://images.unsplash.com/photo-1516224254461-a082c15f78df?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80",
+    description: "Wireless MouseBrand: Apple Color: BlackButtons: 6Connectivity: Wireless 2.4gTechnology: OpticalVoltage",
+    price: 600,
+    countInStock: 6,
+    rating: 2,
+    numReviews: 15,
+
+  },
+];
+
+module.exports = products;
